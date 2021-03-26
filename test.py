c286e8b5fecf6f058473e3ddd871842a48b75d3f
Yann Zerlaut <yann.zerlaut@gmail.com>
2021-03-26 20:43:25 +0100

    testing web scraping from website

---
+++ test.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
+
+page = 'https://www.arte.tv/fr/videos/RC-020451/house-of-cards/'
+req = Request(page)
+html_page = urlopen(req)
+
+
+soup = BeautifulSoup(html_page, "lxml")
+
+links = []
+for link in soup.findAll('a'):
+    l = link.get('href')
+    if page.split('/')[-2] in l:
+        links.append(l)
+        print(l)
+
